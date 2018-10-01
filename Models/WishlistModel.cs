433ee2d2ff97724423506b5907243a81c835fcad
joelscholtz <joelscholtz@hotmail.com>
2018-10-01 14:42:32 +0200

    Models done

--- Models/WishlistModel.cs
+++ Models/WishlistModel.cs
@@ -8,6 +8,11 @@ namespace bytme.Models
     public class WishlistModel
     {
         public int id { get; set; }
+        public int item_id { get; set; }
+        public string description { get; set; }
+        public string long_description { get; set; }
+        public float price { get; set; }
+        public string photo_url { get; set; }
         public int stock { get; set; }
         public DateTime dt_created { get; set; }
     }
