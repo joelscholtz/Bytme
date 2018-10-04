83f0703e8055c38f3fc48fca53703291b1aedea6
joelscholtz <joelscholtz@hotmail.com>
2018-10-04 14:38:41 +0200

    Contact finsihed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using bytme.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace bytme.Controllers
 {
@@ -39,5 +41,62 @@ namespace bytme.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult Contact(ContactModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage sentMail = new MailMessage();
+                    sentMail.IsBodyHtml = true;
+                    // TODO Fix HTML weergave
+                    sentMail.Body = " <div style='width:95%;'> " +
+                                    "<br><h2> Our customer support will contact you as soon as possible</h2><br>" +
+                                    " <table style='font-family: arial,sans-serif;border-collapse: collapse;width:75%; '>" +
+                                    " <tr> " +
+                                    " <th style='border: 1px solid #dddddd;text-align: left; padding:8px;font-size: 14pt;'>Name:</th>" +
+                                    " <th style='border: 1px solid #dddddd;text-align: left; padding:8px;font-size: 14pt;'>Email:</th>" +
+                                    " <th style='border: 1px solid #dddddd;text-align: left; padding:8px;font-size: 14pt;'>Subject:</th>" +
+                                    " <th style='border: 1px solid #dddddd;text-align: left; padding:8px;font-size: 14pt;'>Message:</th>" +
+                                    " </tr>" +
+                                    " <tr>" +
+                                    " <td style='border: 1px solid #dddddd;text-align: left; padding:8px;'>" + c.name + "</td>" +
+                                    " <td style='border: 1px solid #dddddd;text-align: left; padding:8px;'>" + c.email + "</td>" +
+                                    " <td style='border: 1px solid #dddddd;text-align: left; padding:8px;'>" + c.subject + "</td>" +
+                                    " <td style='border: 1px solid #dddddd;text-align: left; padding:8px;'>" + c.content + "</td>" +
+                                    "</table><br>" +
+                                    "It's also possible to contact us via: 010 - 1234567 from monday till friday between 9:00 and 17:00." +
+                                    "</div>";
+
+                    sentMail.From = new MailAddress("sayoswebshop@gmail.com");
+                    sentMail.To.Add(c.email);
+                    sentMail.To.Add("sayoswebshop@gmail.com");
+                    sentMail.Subject = c.subject;
+
+                    SmtpClient client = new SmtpClient();
+                    client.Host = "smtp.gmail.com";
+                    client.Port = 587;
+                    client.UseDefaultCredentials = false;
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential("sayoswebshop@gmail.com", "PonyParkSlagHaren1234");
+
+
+                    client.Send(sentMail);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thanks for the message, we will contact you as soon as possible! ";
+                }
+                catch (Exception err)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $"There was a error: {err.Message}";
+                }
+            }
+
+            return View();
+        }
+
     }
 }
