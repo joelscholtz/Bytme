2ba568a9c946d6d1e83b980ed7b5e43808a5f687
Floorrrrr <35264480+FloorVermaat@users.noreply.github.com>
2018-10-28 20:46:31 +0100

    General Information Page Finished
    - edited: About (General Information Page) view
    - added:
        * HelpController
        * Delivery information page view
        * Orders information page view
        * Payments information page view
        * Return information page view
        * Sizing information page view

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bytme.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Delivery()
+        {
+            return View();
+        }
+
+        public IActionResult Payments()
+        {
+            return View();
+        }
+
+        public IActionResult Return()
+        {
+            return View();
+        }
+
+        public IActionResult Sizing()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
