3081fc3c3d47cd4f40699f5ba8a27c22add338f4
Floorrrrr <35264480+FloorVermaat@users.noreply.github.com>
2018-10-29 02:09:25 +0100

    Added correct regexs for address information
    Tried to make the input viewable after inserting the data into the database, but couldn't figure it out. Also, too tired to continue.

--- Areas/Identity/Pages/Account/Manage/Address.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Address.cshtml.cs
@@ -40,31 +40,35 @@ namespace bytme.Areas.Identity.Pages.Account.Manage
             [Required]
             [DataType(DataType.Text)]
             [Display(Name = "Name")]
+            [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "A name can only contain letters.")]
             [StringLength(100, ErrorMessage = "Invalid input. Maximum is 100 characters.")]
             public string name { get; set; }
 
             [Required]
             [DataType(DataType.Text)]
             [Display(Name = "Surname")]
+            [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "A surname can only contain letters.")]
             [StringLength(100, ErrorMessage = "Invalid input. Maximum is 100 characters.")]
             public string surname { get; set; }
 
             [Required]
             [DataType(DataType.Text)]
             [Display(Name = "Street")]
+            [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "A street name can only contain letters. If you want to add a house number, do it in the next field.")]
             [StringLength(48, ErrorMessage = "The longest street name in the Netherlands is 48 characters.")]
             public string street { get; set; }
 
             [Required]
             [DataType(DataType.Text)]
             [Display(Name = "House Number")]
-            [StringLength(5, ErrorMessage = "The longest house number in the Netherlands is 5 characters.")]
+            [RegularExpression(@"^[0-9]{1,5}([a-z]{1,2})?$", ErrorMessage ="A house number starts with at least 1 digit. House number addition is possible.")]
+            [StringLength(7, ErrorMessage = "The longest house number in the Netherlands is 7 characters.")] // for example: 18999AA
             public string streetnumber { get; set; }
             
-
             [Required]
             [DataType(DataType.Text)]
             [Display(Name = "City")]
+            [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "A city can only contain letters.")]
             [StringLength(28, ErrorMessage = "The longest place name in the Netherlands is 28 characters.")]
             public string city { get; set; }
 
@@ -74,6 +78,18 @@ namespace bytme.Areas.Identity.Pages.Account.Manage
             [RegularExpression(@"^[1-9][0-9]{3}\s?[a-zA-Z]{2}$", ErrorMessage = "Invalid zip, for example: 1234AB")]
             public string zipcode { get; set; }
         }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            return Page();
+        }
+
 
 
 
