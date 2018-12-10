d60dda7293d3bc86f4a22f51901d6de6b41a1e99
Chérie <cherie-@hotmail.nl>
2018-12-10 11:44:26 +0100

    Admin stuff(not done)
    -idk nathan wil het zien ¯\_(ツ)_/¯

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using bytme.Data;
+using bytme.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,17 +14,53 @@ namespace bytme.Controllers
 {
     public class ManageController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<UserModel> _userManager;
 
-        [Authorize(Roles = "Admin")]
-        public IActionResult Index()
+        public ManageController(ApplicationDbContext db, UserManager<UserModel> userManager)
         {
-            return View();
+            _db = db;
+            _userManager = userManager;
         }
 
+        // user overview
         [Authorize(Roles = "Admin")]
-        public IActionResult Products()
+        public ViewResult Index(string searchKeyU)
         {
-            return View();
+
+            var users = from u in _db.Users
+                        select u;
+            if (!String.IsNullOrEmpty(searchKeyU))
+            {
+                //searches for items containing input searchkey
+                // .ToUpper() is used to make search non-case sensitive
+                users = _userManager.Users.Where(u => u.Id.ToUpper().Contains(searchKeyU.ToUpper())
+                                       || u.name.ToUpper().Contains(searchKeyU.ToUpper())
+                                       || u.surname.ToUpper().Contains(searchKeyU.ToUpper())
+                                       || u.Email.ToUpper().Contains(searchKeyU.ToUpper()));
+            }
+            return View(users.ToList());
+        }
+
+
+        // product overview
+        [Authorize(Roles = "Admin")]
+        public ViewResult Products(string searchKeyP)
+        {
+            var products = from item in _db.Items
+                           select item;
+            if (!String.IsNullOrEmpty(searchKeyP))
+            {
+
+                //searches for items containing input searchkey
+                products = _db.Items.Where(item => item.id.ToString().ToUpper().Contains(searchKeyP.ToUpper())
+                                        || item.description.ToUpper().Contains(searchKeyP.ToUpper())
+                                        || item.price.ToString().ToUpper().Contains(searchKeyP.ToUpper())
+                                        || item.quantity.ToString().ToUpper().Contains(searchKeyP.ToUpper())
+                                        || item.gender.ToUpper().Contains(searchKeyP.ToUpper()));
+            }
+
+            return View(products.ToList());
         }
 
         [Authorize(Roles = "Admin")]
@@ -28,5 +68,39 @@ namespace bytme.Controllers
         {
             return View();
         }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult UserEdit()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult UserDelete()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult ProductEdit()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult ProductDelete()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
+
+            return View();
+        }
+
     }
 }
