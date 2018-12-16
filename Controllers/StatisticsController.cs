5815b6a48e54d639ff74f664469b6be916892736
Floor <floorvermaat@hotmail.com>
2018-12-16 21:45:52 +0100

    I hope this shit works

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using bytme.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace bytme.Controllers
 {
-    public class StatisticsController
+    public class StatisticsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public StatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            var months = _context.Users.ToList().Select(o => o.dt_created.ToString("MMMM")).ToList();
+            var users = _context.Users.Count();
+            ViewBag.Users = users;
+            ViewBag.Months = string.Join(", ", months);
+
+            return View();
+        }
     }
 }
