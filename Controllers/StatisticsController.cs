3891cdee8abacededd34b2c6c249bc55b4affb53
Floor <floorvermaat@hotmail.com>
2018-12-16 22:41:39 +0100

    nogmeer statistieken schijt

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -20,10 +20,19 @@ namespace bytme.Controllers
         [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
+            //Users
             var months = _context.Users.ToList().Select(o => o.dt_created.ToString("MMMM")).ToList();
             var users = _context.Users.Count();
             ViewBag.Users = users;
             ViewBag.Months = string.Join(", ", months);
+
+            //Moneymoneymomey
+            var moneymonths = _context.OrderHistories.ToList().Select(o => o.dt_created.ToString("MMMM")).ToList();
+            //var moneys = _context.OrderHistories.Select(o => o.price_payed.ToString()).ToList();
+            float kutgeld = _context.OrderHistories.Sum(o => o.price_payed);
+
+            ViewBag.Money = string.Join(", ", kutgeld);
+            ViewBag.MoneyMonths = string.Join(", ", moneymonths);
 
             return View();
         }
