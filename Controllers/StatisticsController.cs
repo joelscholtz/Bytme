472c18251f08d84ee97c32eb08f2b3fe7ccdde87
Floorrrrr <35264480+FloorVermaat@users.noreply.github.com>
2019-01-11 17:59:45 +0100

    Statistics final

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -21,18 +21,45 @@ namespace bytme.Controllers
         public IActionResult Index()
         {
             //Users
-            var months = _context.Users.ToList().Select(o => o.dt_created.ToString("MMMM")).ToList();
-            var users = _context.Users.Count();
-            ViewBag.Users = users;
-            ViewBag.Months = string.Join(", ", months);
+            ViewBag.UsersJAN = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "januari").Count();
+            ViewBag.UsersFEB = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "februari").Count();
+            ViewBag.UsersMAR = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "maart").Count();
+            ViewBag.UsersAPR = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "april").Count();
+            ViewBag.UsersMAY = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "mei").Count();
+            ViewBag.UsersJUN = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "juni").Count();
+            ViewBag.UsersJUL = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "juli").Count();
+            ViewBag.UsersAUG = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "augustus").Count();
+            ViewBag.UsersSEP = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "september").Count();
+            ViewBag.UsersOCT = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "oktober").Count();
+            ViewBag.UsersNOV = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "november").Count();
+            ViewBag.UsersDEC = _context.Users.Where(o => o.dt_created.ToString("MMMM") == "december").Count();
+
+            if (ViewBag.UsersJAN == 0) { ViewBag.UsersJAN = null; }
+            if (ViewBag.UsersFEB == 0) { ViewBag.UsersFEB = null; }
+            if (ViewBag.UsersMAR == 0) { ViewBag.UsersMAR = null; }
+            if (ViewBag.UsersAPR == 0) { ViewBag.UsersAPR = null; }
+            if (ViewBag.UsersMAY == 0) { ViewBag.UsersMAY = null; }
+            if (ViewBag.UsersJUN == 0) { ViewBag.UsersJUN = null; }
+            if (ViewBag.UsersJUL == 0) { ViewBag.UsersJUL = null; }
+            if (ViewBag.UsersAUG == 0) { ViewBag.UsersAUG = null; }
+            if (ViewBag.UsersSEP == 0) { ViewBag.UsersSEP = null; }
+            if (ViewBag.UsersOCT == 0) { ViewBag.UsersOCT = null; }
+            if (ViewBag.UsersNOV == 0) { ViewBag.UsersNOV = null; }
+            if (ViewBag.UsersDEC == 0) { ViewBag.UsersDEC = null; }
 
             //Moneymoneymomey
-            var moneymonths = _context.OrderHistories.ToList().Select(o => o.dt_created.ToString("MMMM")).ToList();
-            //var moneys = _context.OrderHistories.Select(o => o.price_payed.ToString()).ToList();
-            float kutgeld = _context.OrderHistories.Sum(o => o.price_payed);
-
-            ViewBag.Money = string.Join(", ", kutgeld);
-            ViewBag.MoneyMonths = string.Join(", ", moneymonths);
+            ViewBag.MoneyJAN = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "januari").Sum(o => o.price_payed);
+            ViewBag.MoneyFEB = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "februari").Sum(o => o.price_payed);
+            ViewBag.MoneyMAR = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "maart").Sum(o => o.price_payed);
+            ViewBag.MoneyAPR = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "april").Sum(o => o.price_payed);
+            ViewBag.MoneyMAY = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "mei").Sum(o => o.price_payed);
+            ViewBag.MoneyJUN = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "juni").Sum(o => o.price_payed);
+            ViewBag.MoneyJUL = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "juli").Sum(o => o.price_payed);
+            ViewBag.MoneyAUG = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "augustus").Sum(o => o.price_payed);
+            ViewBag.MoneySEP = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "september").Sum(o => o.price_payed);
+            ViewBag.MoneyOCT = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "oktober").Sum(o => o.price_payed);
+            ViewBag.MoneyNOV = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "november").Sum(o => o.price_payed);
+            ViewBag.MoneyDEC = _context.OrderHistories.Where(o => o.dt_created.ToString("MMMM") == "december").Sum(o => o.price_payed);
 
             return View();
         }
